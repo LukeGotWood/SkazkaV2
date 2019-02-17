fea755e716fb807b0a2cae0c5e90e2800ff19ea0
Luke <l.wood0097@gmail.com>
2019-02-17 23:06:34 +0000

    Revert to working code

--- story.c
+++ story.c
@@ -8,47 +8,47 @@
 // Function for the first section of the story
 void intro() {
 
-    // // Load the first dialogue and run through
-    // setDialogue(beginning);
-    // displayNextMessage();
+    // Load the first dialogue and run through
+    setDialogue(beginning);
+    displayNextMessage();
 
-    // speak(maincharacter, 2);
-    // displayNextMessage();
+    speak(maincharacter, 2);
+    displayNextMessage();
 
-    // speak(stepmother, 2);
-    // displayNextMessage();
+    speak(stepmother, 2);
+    displayNextMessage();
 
-    // speak(maincharacter, 1);
-    // displayNextMessage();
+    speak(maincharacter, 1);
+    displayNextMessage();
 
-    // speak(stepmother, 1);
-    // displayNextMessage();
+    speak(stepmother, 1);
+    displayNextMessage();
 
-    // speak(maincharacter, 1);
-    // displayNextMessage();
+    speak(maincharacter, 1);
+    displayNextMessage();
 
-    // speak(stepmother, 1);
-    // displayNextMessage();
+    speak(stepmother, 1);
+    displayNextMessage();
 
-    // speak(maincharacter, 1);
-    // displayNextMessage();
+    speak(maincharacter, 1);
+    displayNextMessage();
 
-    // clearBackground();
-    // delay(1000);
-    // displayNextMessage();
+    clearBackground();
+    delay(1000);
+    displayNextMessage();
 
-    // // Needs replacing with an image of footsteps
-    // clearBackground();
-    // delay(5000);
-    // displayNextMessage();
+    // Needs replacing with an image of footsteps
+    clearBackground();
+    delay(5000);
+    displayNextMessage();
 
-    // Load the footsteps dialogue
-    if (makeDecision("FOLLOW THE", "FOOTSTEPS?")) {
-        followFootsteps();
-    }
-    else {
-        followBridge();
-    }
+    // // Load the footsteps dialogue
+    // if (makeDecision("FOLLOW THE", "FOOTSTEPS?")) {
+    //     followFootsteps();
+    // }
+    // else {
+    //     followBridge();
+    // }
 
 }
 

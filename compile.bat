e73fac7cf056ca7733b406fa9c6c7d1bc06ca71b
LukeGotWood <l.wood0097@gmail.com>
2019-02-18 17:05:55 +0000

    Updated compile.bat to include new .c files

--- compile.bat
+++ compile.bat
@@ -2,6 +2,7 @@
 REM Compile main files
 "Compiler/bin/lcc" -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o "SkazkaV2/SkazkaV2.o" "SkazkaV2/SkazkaV2.c"
 "Compiler/bin/lcc" -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o "SkazkaV2/music.o" "SkazkaV2/music.c"
+"Compiler/bin/lcc" -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o "SkazkaV2/story.o" "SkazkaV2/story.c"
 
 REM Compile banks
 "Compiler/bin/lcc" -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wf-bo1 -Wf-ba1 -c -o "SkazkaV2/bk1-bkg.o" "SkazkaV2/bk1-bkg.c"
@@ -10,7 +11,7 @@ REM Compile banks
 "Compiler/bin/lcc" -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wf-bo4 -Wf-ba1 -c -o "SkazkaV2/bk4-story.o" "SkazkaV2/bk4-story.c"
 
 REM Wrap them all together
-"Compiler/bin/lcc" -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wl-yt2 -Wl-yo8 -Wl-ya1 -o "SkazkaV2/SkazkaV2.gb" "SkazkaV2/SkazkaV2.o" "SkazkaV2/music.o" "SkazkaV2/bk1-bkg.o" "SkazkaV2/bk2-bkg.o" "SkazkaV2/bk3-bkg.o" "SkazkaV2/bk4-story.o"
+"Compiler/bin/lcc" -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -Wl-yt2 -Wl-yo8 -Wl-ya1 -o "SkazkaV2/SkazkaV2.gb" "SkazkaV2/SkazkaV2.o" "SkazkaV2/music.o" "SkazkaV2/story.o" "SkazkaV2/bk1-bkg.o" "SkazkaV2/bk2-bkg.o" "SkazkaV2/bk3-bkg.o" "SkazkaV2/bk4-story.o"
 
 REM Run the product
 "Emulator/bgb64" "SkazkaV2/SkazkaV2.gb"
